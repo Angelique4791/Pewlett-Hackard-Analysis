260930d857d00fb3b908bcac2d5c8dccd83d6268
Sean Walker <wangelique137@gmail.comgit commit -m added>
2022-05-16 00:35:35 -0500

    Updating SQL file.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,9 +11,9 @@ FROM employees as e
 INNER JOIN titles as tl
 ON (e.emp_no = tl.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 	      AND (tl.to_date = '9999-01-01')
-		  ORDER BY tl.emp_no DESC; 
+		  ORDER BY tl.emp_no ASC;  
+
 
 -- Use Dictinct with Orderby to remove duplicate rows
 -- SELECT DISTINCT ON (______) _____,
@@ -25,3 +25,17 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 -- FROM _______
 -- WHERE _______
 -- ORDER BY _____, _____ DESC;
+
+-- RETIREMENT AGE EMPLOYEES BY UNIQUE TITLE
+-- JOIN employees AND titles TABLES, 
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    tl.title
+INTO unique_titles
+FROM employees as e
+INNER JOIN titles as tl
+ON (e.emp_no = tl.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	      AND (tl.to_date = '9999-01-01')
+		  ORDER BY tl.emp_no ASC;  
